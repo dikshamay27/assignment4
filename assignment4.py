690f0217ff481e9866e28caa6e95c26dc67548bb
dikshamay27 <may27diksha@gmail.com>
2018-06-15 11:44:22 +0530

    file added

---
+++ assignment4.py
@@ -0,0 +1,74 @@
+#tuple
+#wap to create a tuple with dif. data type and do the following operation
+#find the length of tuple
+#t=(1,2,3,4,5)
+#print(t)
+#print(len(t))
+#print(max(t))
+#print(min(t))
+#wap to find product of all ele. of tuple
+
+
+
+
+
+#sets
+#create two sets using user defined values
+#calculate difference b/w two sets
+#print the result of intersection of two sets
+x=int(input("enter the values in set1"))
+a=input("1")
+b=input("2")
+c=input("3")
+s1=set([a,b,c])
+print(s1)
+y=int(input("enter the value in set2")
+d=input("4")
+e=input("5")
+f=input("6")
+s2=set([d,e,f])
+print(s2)
+#calculate the difference b/w two sets
+print(s1-s2)
+#print the result of intersection of two sets
+print(s1&s2)
+
+
+
+
+#dictionary
+#create a dictionary to store name and marks of 10 student
+d={}
+x=int(input("enter the marks of student1"))
+y=input("enter the name of student1")
+d[y]=x
+x=int(input("enter the marks of student2"))
+y=input("enter the name of student2")
+d[y]=x
+x=int(input("enter the marks of student3"))
+y=input("enter the name of student3")
+d[y]=x
+x=int(input("enter the marks of student4"))
+y=input("enter the name of student4")
+d[y]=x
+x=int(input("enter the marks of student5"))
+y=input("enter the name of student5")
+d[y]=x
+x=int(input("enter the marks of student6"))
+y=input("enter the name of student6")
+d[y]=x
+x=int(input("enter the marks of student7"))
+y=input("enter the name of student7")
+d[y]=x
+x=int(input("enter the marks of student8"))
+y=input("enter the name of student8")
+d[y]=x
+
+
+x=int(input("enter the marks of student9"))
+y=input("enter the name of student9")
+d[y]=x
+x=int(input("enter the marks of student10"))
+y=input("enter the name of student10")
+d[y]=x
+print(d)
